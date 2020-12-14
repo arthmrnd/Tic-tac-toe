1feefe2ca824daa632dcf86d4603e76a721a00fd
arthmrnd <theodinksson@gmail.com>
2020-12-14 15:17:50 -0300

    Update Joguinho.cpp

--- Joguinho.cpp
+++ Joguinho.cpp
@@ -1,209 +1,258 @@
-#include<bits/stdc++.h>
-
+#include <iostream>
+#include<list>
+#include <cstdlib>
+#include<string>
+#include <ctime>
+ 
 using namespace std;
-
-// define os players
-#define COMPUTER 1
-#define HUMAN 2
-
-#define SIDE 3 // tamanho do tabuleiro
-
-//computer irá usar o 'O'
-#define COMPUTERMOVE 'O'
-//humano irá usar o 'x'
-#define HUMANMOVE 'X'
-
-//função para mostrar o tabuleiro
-void showboard (char board[][SIDE])
-{
-    printf("\n\n");
-    printf("\t\t\t %c | %c | %c \n", board[0][0], board[0][1], board[0][2]);
-    printf("\t\t\t----------------\n");
-    printf("\t\t\t %c | %c | %c \n", board[1][0], board[1][1], board[1][2]);
-    printf("\t\t\t----------------\n");
-    printf("\t\t\t %c | %c | %c \n", board[2][0], board[2][1], board[2][2]);
-    return;
-}
-
-//Uma função para mostrar as instruções
-void showinstructions ()
-{
-    
-    printf("\t\t\t Jogo da Velha\n\n");
-    printf("Escolha a celula de 1 a 9 no tabuleiro abaixo\n\n");
-    printf("\t\t\t 1 | 2 | 3 \n");
-    printf("\t\t\t----------------\n");
-    printf("\t\t\t 4 | 5 | 6 \n");
-    printf("\t\t\t----------------\n");
-    printf("\t\t\t 7 | 8 | 9 \n");
-    printf("\t\t\t----------------\n");
-
-    printf("\t-\t-\t-\t-\t-\t-\t-\t-\t-\t-\t-\t-\n");
-
-    return;
-}
-
-//função que inicialisa o jogo
-void initialise (char board[][SIDE], int moves[])
-{
-    //inicia um numero random
-    srand(time(NULL));
-    //inicia o tabuleiro vazio
-    for (int i = 0; i < SIDE; i++)
-    {
-        /* code */
-        for (int j = 0; j < SIDE; j++)
-        {
-            /* code */
-            board[i][j] = ' ';
-
-        }
-        
+ 
+typedef struct{
+    int *row;
+}WinList;
+ 
+ 
+class Player {
+private:
+    string name;
+    int score;
+public:
+    Player() :Player {""}{}
+    Player(string n) :score{0}, name{n}{}
+ 
+    void won(){
+        //increment the score
+        score++;
     }
-    
-    //preenche o tabuleiro com numeros
-    for (int i = 0; i < SIDE*SIDE; i++)
-    {
-        /* code */
-        moves[i] = i;
+    int getScore(){ return this->score;}
+ 
+    string getName(){ return this->name;}
+};
+ 
+class Game {
+private:
+    char board[9];
+    int emptyIndex[9];
+    int gameOn, againstComputer;
+    int emptyCount;
+    WinList winlist[8];
+ 
+    void displayBoard(){
+        cout <<endl;
+        cout << "   |   |   "<<endl;
+        cout << " "<< board[0] <<" | "<<board[1]<<" | "<<board[2]<<endl;
+        cout << "   |   |   "<<endl;
+        cout << "-----------"<<endl;
+        cout << "   |   |   "<<endl;
+        cout << " "<< board[3] <<" | "<<board[4]<<" | "<<board[5]<<endl;
+        cout << "   |   |   "<<endl;
+        cout << "-----------"<<endl;
+        cout << "   |   |   "<<endl;
+        cout << " "<< board[6] <<" | "<<board[7]<<" | "<<board[8]<<endl;
+        cout << "   |   |   "<<endl;
+        cout <<endl;
     }
-
-    // torna as jogadas aleatorias
-    random_shuffle(moves, moves + SIDE*SIDE);
-
-    return;
-}
-
-//declara o vencedor do jogo
-
-void declarewinner (int whoseturn)
+ 
+    void computerInput(){
+        int pos;
+        pos = rand()%10;
+        if(emptyIndex[pos] == 1){
+            if(emptyCount < 0)
+                return;
+            computerInput();
+        } else {
+            cout<< "Computer choose: " << pos+1 << endl;
+            emptyIndex[pos] =1;
+            emptyCount-=1;
+            board[pos] = 'O';
+        }
+ 
+    }
+ 
+    void playerInput(Player &player){
+        int pos;
+        cout << endl;
+        cout << "\t" << player.getName() <<" Turn: ";
+        cout <<"\t Enter the position " << endl;
+        cin >> pos;
+        pos -=1;
+        if(emptyIndex[pos] == 1){
+            cout << "-----Position not empty-------"<< endl;
+            playerInput(player);
+        } else {
+            emptyIndex[pos] =1;
+            emptyCount-=1;
+            player.getName().compare("Player I") == 0 ? board[pos] ='X': board[pos] ='O';
+        }
+ 
+    }
+ 
+    void checkWin(Player &p1,Player &p2){
+        int i,j,k;
+        bool flag = false;
+        char first_symbol;
+        for(i=0; i<8; i++){
+            first_symbol = board[winlist[i].row[0]];
+ 
+            if((first_symbol != 'X') && (first_symbol != 'O')){
+                flag = false;
+                continue;
+            }
+            flag = true;
+            for(j=0;j<3;j++){
+                if(first_symbol != board[winlist[i].row[j]]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                gameOn = 0;
+                if(first_symbol == 'X'){
+                    cout << "-----------------------"<< endl;
+                    cout << "\t Player I WON"<< endl;
+                    cout << "-----------------------"<< endl;
+                    p1.won();
+                } else {
+                    p2.won();
+                    if(againstComputer){
+                        cout << "-----------------------"<< endl;
+                        cout << "\t Computer WON"<< endl;
+                        cout << "-----------------------"<< endl;
+                    } else {
+                        cout << "-----------------------"<< endl;
+                        cout << "\t Player II WON"<< endl;
+                        cout << "-----------------------"<< endl;
+ 
+                    }
+                }
+                displayScore(p1,p2);
+                break;
+            }
+        }
+    }
+ 
+        void play(Player &p1,Player &p2){
+            char rematch ='\0';
+            int hand = 0;
+            gameOn =1;
+            displayBoard();
+            while((emptyCount > 0) && (gameOn != 0)){
+ 
+                if(againstComputer)
+                    hand == 1 ? computerInput(): playerInput(p2);
+                else
+                    hand == 1 ? playerInput(p1): playerInput(p2);
+                hand= !hand;
+                displayBoard();
+                checkWin(p1,p2);
+            }
+            if (emptyCount <=0){
+                cout << "      -----------------------"<< endl;
+                cout << "\t No WINNER"<< endl;
+                cout << "      -----------------------"<< endl;
+            }
+            cout<< endl;
+            cout << "Rematch Y/N: ";
+            cin >> rematch;
+            if((rematch == 'Y')||(rematch == 'y')){
+                init();
+                play(p1,p2);
+            }
+ 
+        }
+    void displayScore(Player &p1, Player &p2){
+        cout << endl;
+        cout << "\t SCORE: \t";
+        if(againstComputer)
+            cout<<" Player I: " <<p1.getScore()<<" \t Computer: "<<p2.getScore()<< endl;
+        else
+            cout<<" Player I: " <<p1.getScore()<<" \t Player II: "<<p2.getScore()<< endl;
+    }
+ 
+public:
+    Game(): emptyCount{0}, gameOn{1}, againstComputer{0}{
+        init();
+        winlist[0].row = new int[3]{0,1,2};
+        winlist[1].row = new int[3]{3,4,5};
+        winlist[2].row = new int[3]{6,7,8};
+        winlist[3].row = new int[3]{0,3,6};
+        winlist[4].row = new int[3]{1,4,7};
+        winlist[5].row = new int[3]{2,5,8};
+        winlist[6].row = new int[3]{0,4,8};
+        winlist[7].row = new int[3]{2,4,6};
+    }
+ 
+    void init(){
+        gameOn = 1;
+ 
+        emptyCount =0;
+        srand(time(0));
+        for(size_t i=0; i<10; i++){
+            emptyIndex[i] = 0;
+            board[i] = (i+1) +'0';
+            emptyCount++;
+        }
+        emptyCount--;
+    }
+ 
+    void onePlayerGame(){
+        //Creating Player
+        Player p("Player I");
+        Player c("Computer");
+        cout << "       -----------------------"<< endl;
+        cout << "\t Player I: X \t Computer: O"<< endl;
+        cout << "       -----------------------"<< endl;
+        cout << endl;
+        againstComputer = 1;
+        play(c,p);
+ 
+    }
+ 
+    void twoPlayerGame(){
+        //Creating Player
+        Player p("Player I");
+        Player c("Player II");
+        cout << "       -----------------------"<< endl;
+        cout << "\t Player I: X \t Player II: O"<< endl;
+        cout << "       -----------------------"<< endl;
+        cout << endl;
+        againstComputer = 0;
+        play(c,p);
+    }
+};
+ 
+ 
+int main()
 {
-    if (whoseturn == COMPUTER)
-    {
-        printf("O Computador Ganhou!\n");
+    int ch;
+ 
+    while(1){
+        cout<< "      ----------MENU----------" << endl;
+        cout << "\t 1. 1 Player game" <<endl;
+        cout << "\t 2. 2 Player game" <<endl;
+        cout << "\t 3. To exit " <<endl;
+        cout <<"      ------------------------" << endl;
+        cout << endl;
+        cout <<"\t Select an option" << endl;
+        cin >> ch;
+        switch(ch){
+        case 1:{
+            Game *game = new Game;
+            game->init();
+            game->onePlayerGame();
+            }
+            break;
+        case 2:{
+            Game *game = new Game;
+            game->init();
+            game->twoPlayerGame();
+            }
+            break;
+        case 3:
+            return 0;
+        default:
+            cout << "OOPs Invalid Option! TRY AGAIN";
+        }
+ 
     }
-    else
-    {
-        printf("Computador 2 ganhaste!\n");
-    }
-     return;
-}
-
-// função que retorna um booleano se para as se cruza com as jogadas dos players
-bool rowcrossed(char board[][SIDE])
-{
-    for (int i = 0; i < SIDE; i++)
-    {
-        if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
-        {
-            return(true);
-        }
-        
-    }
-    return(false);
-}
-
-// funçao se a coluna se cruzam 
-bool columncrossed(char board[][SIDE])
-{
-    for (int i = 0; i < SIDE; i++)
-    {
-        if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
-        {
-            return(true);
-        }
-        
-    }
-    return(false);
-}
-
-//função para se a diagonal se cruzarem com a jogada dos players
-bool diagonalcrossed(char board[][SIDE])
-{
-    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
-        {
-            return(true);
-        }
-    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
-        {
-            return(true);
-        }
-    return(false);
-}
-
-//função que diz se o jogo terminou
-bool gameover(char board[][SIDE])
-{
-    return (rowcrossed(board) || columncrossed(board) || diagonalcrossed(board));
-} 
-
-//função par jogar o joguineo
-void playvelha(int whoseturn)
-{
-    // tabuleiro 3x3
-    char board [SIDE][SIDE];
-    int moves[SIDE*SIDE];
-    
-    // iniciar o jogo
-    initialise(board, moves);
-
-    //chamando a função que mostra as instruçoes
-    showinstructions();
-
-    int moveindex = 0, x, y;
-
-    //jogando o jogo
-
-    while (gameover(board) == false && moveindex != SIDE*SIDE)
-    {
-        if (whoseturn == COMPUTER)
-        {
-            x = moves[moveindex] / SIDE;
-            y = moves[moveindex] % SIDE;
-            board [x][y] = COMPUTERMOVE;
-            printf("\nComputador colocou %c no espaco %d\n",COMPUTERMOVE, moves[moveindex]+1);
-            showboard(board);
-            moveindex++;
-            whoseturn = HUMAN;
-            
-        }
-        else if (whoseturn = HUMAN)
-        {
-            x = moves[moveindex] / SIDE;
-            y = moves[moveindex] % SIDE;
-            board [x][y] = HUMANMOVE;
-            printf("\nComputador 2 colocou %c no espaco %d\n", HUMANMOVE, moves[moveindex]+1);
-            showboard(board);
-            moveindex++;
-            whoseturn = COMPUTER;
-        }
-        
-    }
-    //se o jogo empatar
-    if (gameover(board) == false && moveindex == SIDE*SIDE)
-    {
-        printf("Empatou\n");
-    }
-    //se tiver um vencedor
-    else
-    {
-        if (whoseturn == COMPUTER)
-        {
-            whoseturn = HUMAN;
-        }
-        else if (whoseturn == HUMAN)
-        {
-            whoseturn = COMPUTER;
-        }
-        
-        declarewinner(whoseturn);
-        return;    
-    }
-}
-
-int main ()
-{
-    playvelha(COMPUTER);
-    printf("\n\tAte mais ver\n");
-    return(0);
+    return 0;
 }
